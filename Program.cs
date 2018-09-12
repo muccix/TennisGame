a271b9c7aad3488e52d9bf3c2a2210e65fd0bf33
Roberto Mucci <r.mucci@cineca.it>
2018-09-12 23:40:53 +0200

    Add VS Code project

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TennisGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var playerA = new Player(Players.PlayerA, "Roger");
+            var playerB = new Player(Players.PlayerB, "Rafa");
+            var randomPointResultEngine = new RandomPointResultEngine();
+            var gameScoreManager = new GameScoreManager(playerA, playerB);
+
+            var gameEngine = new GameEngine(randomPointResultEngine,
+                                            gameScoreManager,
+                                            playerA,
+                                            playerB);
+
+            Console.WriteLine("Welcome to the Tennis Game!");
+            System.Console.WriteLine("Roger vs Rafa: who is going to win this game?");
+
+            gameEngine.PlayGame();
+
+        }
+    }
+}
