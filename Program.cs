820d67c3bee12c507e393729202c2b769a11896f
Roberto Mucci <r.mucci@cineca.it>
2018-09-13 00:21:49 +0200

    Improve console output

--- Program.cs
+++ Program.cs
@@ -6,8 +6,8 @@ namespace TennisGame
     {
         static void Main(string[] args)
         {
-            var playerA = new Player(Players.PlayerA, "Roger");
-            var playerB = new Player(Players.PlayerB, "Rafa");
+            var playerA = new Player(Players.PlayerA, "ROGER");
+            var playerB = new Player(Players.PlayerB, "RAFA");
             var randomPointResultEngine = new RandomPointResultEngine();
             var gameScoreManager = new GameScoreManager(playerA, playerB);
 
@@ -15,12 +15,13 @@ namespace TennisGame
                                             gameScoreManager,
                                             playerA,
                                             playerB);
-
+            Console.Clear();
+            System.Console.WriteLine("\n*****************************");
             Console.WriteLine("Welcome to the Tennis Game!");
-            System.Console.WriteLine("Roger vs Rafa: who is going to win this game?");
+            System.Console.WriteLine($"{playerA.Name} vs {playerB.Name}: who is going to win this game?");
+            System.Console.WriteLine("*****************************\n");
 
             gameEngine.PlayGame();
-
         }
     }
 }
