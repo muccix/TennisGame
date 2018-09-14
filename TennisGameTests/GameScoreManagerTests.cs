6c09433981742230b4add015fc6440884216f6cb
Roberto Mucci <muccix@gmail.com>
2018-09-14 11:05:50 +0200

    Added Test solution

---
+++ TennisGameTests/GameScoreManagerTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TennisGame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TennisGame.Tests
+{
+    [TestClass()]
+    public class GameScoreManagerTests
+    {
+        [TestMethod()]
+        public void GameScoreManagerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateScoreTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CheckGameWinnerTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
