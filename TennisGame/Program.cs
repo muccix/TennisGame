2e738a822f729701e3028604ffb0af02e53d91a8
muccix <muccix@gmail.com>
2018-09-18 10:24:22 +0200

    Added try/catch in Program.cs

--- TennisGame/Program.cs
+++ TennisGame/Program.cs
@@ -4,18 +4,26 @@
     {
         static void Main(string[] args)
         {
-            var playerA = new Player("ROGER");
-            var playerB = new Player("RAFA");
-            var randomPointResultEngine = new RandomPointResultEngine();
-            var gameScoreManager = new GameScoreManager();
-            var consoleUserInterface = new ConsoleUserInterface();
+            try
+            {
+                var playerA = new Player("");
+                var playerB = new Player("ROGER");
+                var randomPointResultEngine = new RandomPointResultEngine();
+                var gameScoreManager = new GameScoreManager();
+                var consoleUserInterface = new ConsoleUserInterface();
 
-            var gameEngine = new GameEngine(randomPointResultEngine,
-                                            gameScoreManager,
-                                            consoleUserInterface,
-                                            playerA,
-                                            playerB);
-            gameEngine.PlayGame();
+                var gameEngine = new GameEngine(randomPointResultEngine,
+                                                gameScoreManager,
+                                                consoleUserInterface,
+                                                playerA,
+                                                playerB);
+                gameEngine.PlayGame();
+            }
+            catch (System.Exception ex)
+            {
+                System.Console.WriteLine(ex);
+            }
+
         }
     }
 }
